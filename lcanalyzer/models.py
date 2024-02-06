822466e87bbfa3814ce7a06d9842bf68e564eb61
deckerkf <deckerkfrench@gmail.com>
2024-02-06 12:27:13 -0600

    added pylint to github actions

--- lcanalyzer/models.py
+++ lcanalyzer/models.py
@@ -2,8 +2,8 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-The lightcurves are saved in a table (2D array) where each row corresponds to a single observation. 
-Depending on the dataset (LSST or Kepler), a table can contain observations of a single or several objects, 
+The lightcurves are saved in a table (2D array) where each row corresponds to a single observation.
+Depending on the dataset (LSST or Kepler), a table can contain observations of a single or several objects,
 in a single or different bands.
 
 Functions:
@@ -17,6 +17,7 @@ import pandas as pd
 import numpy as np
 from astropy.timeseries import LombScargle
 
+
 def load_dataset(filename):
     """Load a table from CSV file.
     :param filename: The name of the .csv file to load
@@ -25,7 +26,7 @@ def load_dataset(filename):
     return pd.read_csv(filename)
 
 
-def mean_mag(data,mag_col):
+def mean_mag(data, mag_col):
     """Calculate the mean magnitude of a lightcurve
     :param data: pd.DataFrame with observed magnitudes for a single source.
     :param mag_col: a string with the name of the column for calculating the mean value.
@@ -34,7 +35,7 @@ def mean_mag(data,mag_col):
     return data[mag_col].mean()
 
 
-def max_mag(data,mag_col):
+def max_mag(data, mag_col):
     """Calculate the max magnitude of a lightcurve
     :param data: pd.DataFrame with observed magnitudes for a single source.
     :param mag_col: a string with the name of the column for calculating the max value.
@@ -43,13 +44,14 @@ def max_mag(data,mag_col):
     return data[mag_col].max()
 
 
-def min_mag(data,mag_col):
+def min_mag(data, mag_col):
     """Calculate the min magnitude of a lightcurve
     :param data: pd.DataFrame with observed magnitudes for a single source.
     :param mag_col: a string with the name of the column for calculating the min value.
     :returns: The min value of the column.
     """
     return data[mag_col].min()
+
 
 def calc_stats(lc, bands, mag_col):
     # Calculate max, mean and min values for all bands of a light curve
@@ -62,12 +64,13 @@ def calc_stats(lc, bands, mag_col):
         stats[b] = stat
     return pd.DataFrame.from_records(stats)
 
-def normalize_lc(df,mag_col):
+
+def normalize_lc(df, mag_col):
     # Normalize a light curve
     if any(df[mag_col].abs() > 90):
-        raise ValueError(mag_col+' contains values with abs() larger than 90!')
-    min = min_mag(df,mag_col)
-    max = max_mag((df-min),mag_col)
-    lc = (df[mag_col]-min)/max
+        raise ValueError(mag_col + " contains values with abs() larger than 90!")
+    min_data = min_mag(df, mag_col)
+    max_data = max_mag((df - min), mag_col)
+    lc = (df[mag_col] - min_data) / max_data
     lc = lc.fillna(0)
     return lc
